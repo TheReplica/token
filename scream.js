754d9a24b6eff437fc04c37db759323676d23027
TheReplica <68759260+TheReplica@users.noreply.github.com>
2020-10-15 19:32:03 +0300

    Add files via upload

---
+++ scream.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { Client, RichEmbed } = require("discord.js");
+const app = express();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+const http = require("http");
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get("http://boost-aktif.glitch.me/");
+}, 3000);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const data = new Map();
+
+const scream = new Discord.Client();
+const scream1 = new Discord.Client();
+const scream2 = new Discord.Client();
+const scream3 = new Discord.Client();
+const scream4 = new Discord.Client();
+const scream5 = new Discord.Client();
+const scream6 = new Discord.Client();
+const scream7 = new Discord.Client();
+const scream8 = new Discord.Client();
+const scream9 = new Discord.Client();
+const scream10 = new Discord.Client();
+const scream11 = new Discord.Client();
+const scream12 = new Discord.Client();
+const scream13 = new Discord.Client();
+const scream14 = new Discord.Client();
+const scream15 = new Discord.Client();
+const scream16 = new Discord.Client();
+const scream17 = new Discord.Client();
+const scream18 = new Discord.Client();
+const scream19 = new Discord.Client();
+const scream20 = new Discord.Client();
+
+
+scream.login("NzYzNDUzMjcwMDc2Njg2MzU3.X337uw.LJ6V3yOZAPyVBksM8rscfkJBRlI");
+scream1.login("NzYzNDgyNTQ5MjUwMTYyNzU4.X34gug.dLA9P6QlPBV81UfnsD_uhLNVPdo");
+scream2.login("NzYzNDk0OTI3MDUyMDQ2MzY2.X34iNw.Rh52_rCRl6zh-MQopKoSdS-NhUc");
+scream3.login("NzYzNDk1NTgwNzAxODE4OTAw.X34izQ.OsZMim5t81695Z4lAy85__dhMjA");
+scream4.login("NzYzNDk2MzM4NjUzNTc3MjY4.X34jdA.ZLM2xS9A-pO87m7PSCWRaL4oTD8");
+scream5.login("NzYzNDk5Mjg3MDUyNTUwMTg1.X34mJw._RHFpDd-9F7tBBOkj_6u2_pm1Vg");
+scream6.login("NzYzNDk5OTQzMTU5Mzk4NDAw.X34m1A.86H9d3d7dLXPmiJjcWYEAnwE7O0");
+scream7.login("NzYzNTAxNDU2NDQ3NjM1NDg2.X34oRg.sUK71PS9YgQT9hx5OQU8E_xavLE");
+scream8.login("mfa.3BRhrv9tU9Vuy6so6p9n-kTX-dR4aMGhjcI3v0uNU3N8Ly6tLnG3EIw3sOo1jQcdIrKN02pV0ONsGGsTdgF6");//Meee  .d
+scream9.login("NzYzNzg0NDc1NjIzODE3MjE2.X38v0Q.KRK1jo0QTtfYrDGcwlq_vd6CBlo");
+scream10.login("NzYzNzg1MzY5NTQ3NzAyMjg0.X38wyQ.zOaq-2aWwrtwTcuTTWJLxEX7_5w");
+scream11.login("NzYzNzg2MDQxNzYwODA4OTcw.X38xpw.MhJEQxKrgv_I2LOtxohoARUzw8g");
+scream12.login("NzYzNzg3MDg2MzkwMDk5OTY5.X38yPg.aTV362DMkJWgJpv6JS13FuCvVLE");
+scream13.login("NzYzODAyOTIyMTc3MjY1NzA3.X39BmA.kD6j8w8knjgVWZP4o48_jAzh8Io");
+scream14.login("NzYzODA0OTM1NjE0ODkwMDI1.X39C5Q.2vEj9JwtNQqQlLRZDE8zLk4HmtM");
+scream15.login("NzYzODA1NDQwOTQwODM0ODM4.X39Daw.CPXFlQ9Lc6X1w9QvVHgpO_sQYTg");
+scream16.login("NzYzODA1OTQ4ODQxOTUxMjQy.X39EDQ.Zp5kCHmNpBkK_uP8Ya7P4Ff6_Lk");
+scream17.login("NzYzODA3NTgwOTc0ODc0NjM0.X39FYg.TQAlM-XhkpXBS14h6CYwRSJcv2k");
+scream18.login("NzYzODA4OTQyOTQ1ODYxNzI0.X39Gnw.-T0VFK_bKhltltoIqhrO-gjoJ5M");
